d9a350ce960cf6c1d235b0a586caaa54c2e3fe62
student <student@student28.local>
2019-06-05 15:27:59 -0300

    k

--- uniguia/eventos.swift
+++ uniguia/eventos.swift
@@ -9,95 +9,24 @@
 import Foundation
     class Evento {
 
-        let local: String
-        let title: String
+        var local: String
+        var title: String
         
         
         init(local: String, title: String = "") {
-            self.local = local
+           self.local = local
             self.title = title
-    
+
         }
         
     }
     
  
-    class EventoDAO {
-        
-        static func getList() -> [Evento] {
-            return [
-                Evento(local: "Reuniao", title: "12:12"),
-                Evento(local: "Palestra nati", title: "12:12"),
-                Evento(local: "Palestra CCT", title: "12:12"),
-                Evento(local: "Palestra CCS", title: "12:12"),
-                Evento(local: "APAPORRA", title: "12:12")
-            ]
-        }
-        
-    }
-    
-    class baconDAO{
-        
-        static func getBacon() -> [Evento]{
-            guard let url = URL(string: "https://uniguia.mybluemix.net/events") else { return [Evento.init(local: "APAPORA")] }
-                let request:NSMutableURLRequest = NSMutableURLRequest(url:url as URL)
-                request.httpMethod = "GET"
-                 var session = URLSession.shared
-
-            session.dataTaskWithRequest(request as URLRequest, queue: OperationQueue.main){(response, data, error) in }
-            
-            let task = URLSession.shared.dataTask(with: request as URLRequest) {
-                data, response, error in
-                
-                if error != nil {
-                    print("Error = \(String(describing: error))")
-                    return
-                }
-                do {
-                    print("Response=\(String(describing: response))")
-                    
-                    let responseString = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
-                    print("Response data = \(String(describing: responseString))")
-                    
-                    
-                    var json: NSDictionary!
-                    json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as? NSDictionary
-                    
-                    
-                    let returnEventos: NSArray = json["returnEventos"] as! NSArray
-                    
-                    print(returnEventos);
-                    
-                    var local = [String]()
-                    var title = [String]()
-                    
-                    for i in 0 ..< returnEventos.count{
-                        
-                        
-                        let aObject = returnEventos[i] as! [String : AnyObject]
-                       local.append((aObject["fullName"] as! String))
-                        title.append((aObject["fullName"] as! String))
-                        Evento(local: String(describing: local), title: String(describing: title) )
-                        
-                        
-                    }
-                    
-                    
-                }
-                catch {
-                    print("ERROR: \(error)")
-                }
-                
-            }
-            task.resume()
-
-               }
-    
     class EventosDAO {
         
-        static func getEventos (callback: @escaping ((Evento) -> Void)) {
+        static func getEventos () {
             
-            let endpoint: String = "https://uniguia.mybluemix.net/eventos"
+            let endpoint: String = "https://uniguia.mybluemix.net/events"
             
             guard let url = URL(string: endpoint) else {
                 print("Erroooo: Cannot create URL")
@@ -118,15 +47,79 @@ import Foundation
                 
                 DispatchQueue.main.async() {
                     do {
-                        if let json = try JSONSerialization.jsonObject(with: data!, options: []) as? [[String: AnyObject]] {
+                        if (try JSONSerialization.jsonObject(with: data!, options: []) as? [[String: AnyObject]]) != nil {
                             
-                            let evento = Evento(local: json[0]["local"] as! String)
+                            let evento = Evento(local: "", title: "")
                             
                             let nomelocal = evento.local
                             let nometitle = evento.title
                             
-                            print("\(nomelocal) realizado em \(nometitle).")
+                            print("\(nomelocal) realizado em \(nometitle). \(evento.title)")
+                                
                             
+                        }else {
+                            
+                            print("Erro")
+                        }
+                    } catch let error as NSError {
+                        print("Error = \(error.localizedDescription)")
+                    }
+                }
+                
+                
+            })
+            
+            task.resume()
+        }
+    }
+    class EventoDAO {
+        
+        static func getList() -> [Evento] {
+            return [
+                Evento(local: "Reuniao", title: "12:12"),
+                Evento(local: "Palestra nati", title: "12:12"),
+                Evento(local: "Palestra CCT", title: "12:12"),
+                Evento(local: "Palestra CCS", title: "12:12"),
+                Evento(local: "APAPORRA", title: "12:12")
+            ]
+        }
+        
+    }
+
+    
+    class Eventos2DAO {
+        
+        static func getEventos (callback: @escaping ((Evento) -> Void)) {
+            
+            let endpoint: String = "https://uniguia.mybluemix.net/events"
+            
+            guard let url = URL(string: endpoint) else {
+                print("Erroooo: Cannot create URL")
+                return
+            }
+            
+            let urlRequest = URLRequest(url: url)
+            
+            let task = URLSession.shared.dataTask(with: urlRequest, completionHandler: { (data, response, error) in
+                
+                if error != nil {
+                    print("Error = \(String(describing: error))")
+                    return
+                }
+                
+                let responseString = String(data: data!, encoding: String.Encoding.utf8)
+                print("responseString = \(String(describing: responseString))")
+                
+                DispatchQueue.main.async() {
+                    do {
+                        if (try JSONSerialization.jsonObject(with: data!, options: []) as? [[String: AnyObject]]) != nil {
+                            
+                            let evento = Evento(local: "", title: "")
+                            
+                            let nomelocal = evento.local
+                            let nometitle = evento.title
+                            
+                            print("\(nomelocal) realizado em \(nometitle). \(evento.title)")
                             callback(evento)
                             
                         }else {
@@ -144,4 +137,5 @@ import Foundation
             task.resume()
         }
     }
-    }
+    
+    
